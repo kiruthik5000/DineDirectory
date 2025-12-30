f5bfc554423274be074f85cc89190cc36a827915
kiruthik5000 <kiruthikvairam5000@gmail.com>
2025-12-30 18:53:12 +0530

    resolved fully working docker-compose

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -11,11 +11,12 @@ connectDB();
 
 const app =  express();
 app.use(cors({
-    origin: ['*'],
+    origin: '*',
     methods: ['GET','POST','PUT','DELETE'],
     allowedHeaders: ['Content-Type','Authorization'],
     credentials: true
 }));
+app.options('*', cors());
 app.use(express.json());
 
 // simple route
