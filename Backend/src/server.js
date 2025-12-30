7c9aa374de0b4743f3d7ab41e652df2f5d00e724
kiruthik5000 <kiruthikvairam5000@gmail.com>
2025-12-30 18:15:42 +0530

    resolved cors problem

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -10,7 +10,12 @@ dotenv.config();
 connectDB();
 
 const app =  express();
-app.use(cors())
+app.use(cors({
+    origin: ['*'],
+    methods: ['GET','POST','PUT','DELETE'],
+    allowedHeaders: ['Content-Type','Authorization'],
+    credentials: true
+}));
 app.use(express.json());
 
 // simple route
